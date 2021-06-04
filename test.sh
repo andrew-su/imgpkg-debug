ae1e7e37042a48a004a75544fa2348e9e0dfd878
Andrew Su <suan@vmware.com>
2021-06-04 19:18:25 -0400

    Add all lock file

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-set +e
+set +ex
 
 # imgpkg push -b ${TEST_REGISTRY}/imgpkg-debug/staging:1.0 -f package_content/ --lock-output package.lock
 
 mkdir -p release/.imgpkg
 kbld -f release/config/app.yaml --imgpkg-lock-output release/.imgpkg/images.yml
-imgpkg push -b ${TEST_REGISTRY}/imgpkg-debug/bundle2:1.0 -f release/ --lock-output staging.lock
+imgpkg push -b ${TEST_REGISTRY}/imgpkg-debug/release:1.0 -f release/ --lock-output staging.lock
 
 imgpkg copy --lock staging.lock --to-repo ${TEST_REGISTRY}/imgpkg-debug/release --lock-output release.lock
 
 
 
-imgpkg pull --lock package.lock -o extracted_bundle/package
-imgpkg pull --lock staging.lock -o extracted_bundle/staging
-imgpkg pull --lock release.lock -o extracted_bundle/release
+# imgpkg pull --lock package.lock -o extracted_bundle/package
+# imgpkg pull --lock staging.lock -o extracted_bundle/staging
+# imgpkg pull --lock release.lock -o extracted_bundle/release

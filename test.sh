e2d799cc96784ada43550a87444ced4e4abe78f8
Andrew Su <suan@vmware.com>
2021-06-04 19:32:38 -0400

    Update names to be more clear

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ set +ex
 
 # imgpkg push -b ${TEST_REGISTRY}/imgpkg-debug/staging:1.0 -f package_content/ --lock-output package.lock
 
-mkdir -p release/.imgpkg
-kbld -f release/config/app.yaml --imgpkg-lock-output release/.imgpkg/images.yml
-imgpkg push -b ${TEST_REGISTRY}/imgpkg-debug/release:1.0 -f release/ --lock-output staging.lock
+mkdir -p staging/.imgpkg
+kbld -f staging/config/app.yaml --imgpkg-lock-output staging/.imgpkg/images.yml
+imgpkg push -b ${TEST_REGISTRY}/imgpkg-debug/staging:1.0 -f staging/ --lock-output staging.lock
 
 imgpkg copy --lock staging.lock --to-repo ${TEST_REGISTRY}/imgpkg-debug/release --lock-output release.lock
 
 
 
-# imgpkg pull --lock package.lock -o extracted_bundle/package
-# imgpkg pull --lock staging.lock -o extracted_bundle/staging
-# imgpkg pull --lock release.lock -o extracted_bundle/release
+imgpkg pull --lock package.lock -o extracted_bundle/package
+imgpkg pull --lock staging.lock -o extracted_bundle/staging
+imgpkg pull --lock release.lock -o extracted_bundle/release
